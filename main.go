411b26351ad2e63696bd13e567260c16fdf4d1a8
M. Sonntag <michael.p.sonntag@gmail.com>
2016-08-23 11:48:41 +0200

    properly close window

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/mattn/go-gtk/gtk"
+	"github.com/mattn/go-gtk/glib"
 )
 
 func main() {
@@ -11,6 +12,18 @@ func main() {
 
 	gtk.Init(nil)
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+
+	window.SetPosition(gtk.WIN_POS_CENTER)
+
+	window.SetTitle("Dentry")
+
+	// required to end program properly; first string needs to be "destroy", other text is required but can be
+	// set freely.
+	window.Connect("destroy", func(ctx *glib.CallbackContext) {
+		println("Close Dentry.", ctx.Data().(string))
+		gtk.MainQuit()
+	}, "Dentry closed")
+
 	window.ShowAll()
 	gtk.Main()
 }
