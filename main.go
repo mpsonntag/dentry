2082f6c7c77a92d2029f05d4ca030f778004bfe9
M. Sonntag <michael.p.sonntag@gmail.com>
2016-08-23 11:37:38 +0200

    show window

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mattn/go-gtk/gtk"
+)
 
 func main() {
 	fmt.Println("dentry started")
+
+	gtk.Init(nil)
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.ShowAll()
+	gtk.Main()
 }
